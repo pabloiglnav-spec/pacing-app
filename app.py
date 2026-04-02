2f9bf15f1f4f2f0d5992f7389a7138d617e4e6d1
pabloiglnav-spec <pabloiglnav@gmail.com>
2026-04-02 13:14:21 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title="Pacing Triatlón — Pablo Iglesias Navarrete", layout="centered")
+st.set_page_config(page_title="Pacing Triatlón — Pablo Iglesias Navarrete", layout="
 
 # ============================================
 # 1. CARGAR HEADER HTML
